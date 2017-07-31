01a89f289cbb744afb9381ef7177cc6cf8e07f79
virtuNat <janlothan@gmail.com>
2017-07-31 19:02:21 +0800

    Update setup and add pip requirements

--- setup.py
+++ setup.py
@@ -1,20 +1,18 @@
 import sys
 from cx_Freeze import setup, Executable
 
-# Dependencies are automatically detected, but it might need fine tuning.
+# Dependency list.
 build_exe_options = {
     "packages": ["pygame"], 
     "excludes": ["tkinter", "numpy"],
     "include_files": ["textures", "music", "data"]
     }
-
-# GUI applications require a different base on Windows (the default is for a
-# console application).
+# Base for GUI apps on Windows.
 base = "Win32GUI" if sys.platform == "win32" else None
 
 setup(
     name = "pyTetris",
-    version = "1.1.7",
+    version = "1.1.8",
     description = "Python Tetris clone",
     options = {"build_exe": build_exe_options},
-    executables = [Executable("tetris.py", base = base)])
+    executables = [Executable("__main__.py", base = base)])
